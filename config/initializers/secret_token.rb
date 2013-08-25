359c483d106cdd91e4f3b05d3869ad3724b0fa67
abdul k shaik <imran.ngage143@gmail.com>
2013-08-25 03:28:48 -0700

    Created a rails applications and addit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skypebook::Application.config.secret_token = '0a6063e647cc166bcf632734cbb1fc3b6a16207b07a730b11928f5fb392cff2cc623d4c7186c0be200739bf81d916801fb5421ed6769556aa26003221f6836e8'
